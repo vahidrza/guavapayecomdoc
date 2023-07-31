b9c5d6ad9929cf688e4e74acfee1049fe1a6ec95
vahidrza <vrzayev79@gmail.com>
2023-07-31 12:35:39 +0400

    delete comments

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,21 +1,7 @@
 import "./header.css";
-import React /*, { useEffect } */ from "react";
+import React from "react";
 
 function Header() {
-  // useEffect(() => {
-  //   if (localStorage.getItem("lang") === null)
-  //     localStorage.setItem("lang", "en");
-  //   else;
-  //   document
-  //     .getElementById(localStorage.getItem("lang"))
-  //     .classList.add("active");
-  // }, []);
-
-  // let languageSetting = (lang1, lang2) => {
-  //   localStorage.setItem("lang", lang1);
-  //   document.getElementById(lang1).classList.add("active");
-  //   document.getElementById(lang2).classList.remove("active");
-  // };
 
   return (
     <div id="header" className="header">
@@ -30,31 +16,6 @@ function Header() {
           E-commerce Payment Gateway API Specification
         </h1>
       </div>
-
-      {/* <div className="headerChanges">
-        <div className="headerChangesLanguage">
-          <div
-            onClick={() => languageSetting("en", "ru")}
-            id="en"
-            className="headerChangesLanguageOption"
-          >
-            EN
-          </div>
-          <div
-            onClick={() => languageSetting("ru", "en")}
-            id="ru"
-            className="headerChangesLanguageOption"
-          >
-            RU
-          </div>
-        </div>
-
-        <div className="headerChangesPageMode">
-          <div className="headerChangesPageModeElement">Lite</div>
-
-          <div className="headerChangesPageModeElement">Dark</div>
-        </div>
-      </div> */}
     </div>
   );
 }
