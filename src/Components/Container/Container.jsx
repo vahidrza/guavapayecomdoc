e3dc68d9aa78484235f98f551d16fcee5dbe60df
vahidrza <vrzayev79@gmail.com>
2023-08-04 16:48:34 +0400

    added mobile view

--- src/Components/Container/Container.jsx
+++ src/Components/Container/Container.jsx
@@ -1,13 +1,33 @@
-// import React, { useEffect } from "react";
+import React, { useEffect } from "react";
 import "./container.css";
 import Nav from "./Nav/Nav";
 import Main from "./Main/Main";
+import NoDesktop from "./NoDesktop/NoDesktop";
 
 function Container() {
+
+  const mediaQuery = window.matchMedia('(min-width:1200px)');
+  function applyCustomStyles(mql) {
+    if (mql.matches) {
+      return <>
+        <Nav />
+        <Main />
+      </>
+    } else {
+      return <NoDesktop/>
+    }
+  }
+
+  useEffect(() => {
+    applyCustomStyles(mediaQuery);
+      mediaQuery.addListener(applyCustomStyles);
+  },[]);
+  
   return (
     <div className="container">
-      <Nav />
-      <Main />
+      {/* <Nav />
+      <Main /> */}
+      {applyCustomStyles(mediaQuery)}
     </div>
   );
 }
---
+++ src/Components/Container/NoDesktop/NoDesktop — копия.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import './noDesktop.css'
+
+function NoDesktop() {
+    return (
+        <div className='noDesktop' >
+            <div className="logoContainer">
+                <img src={`${process.env.PUBLIC_URL}/assets/img/logo.svg`}
+                    alt="logo" />
+            </div>
+
+            <div className="noDesktopMainContainer">
+                <img src={`${process.env.PUBLIC_URL}/assets/img/noDesktopPhoto.svg`}
+                    alt="logo" />
+            </div>
+
+            <div className="noDesktopParagraph">
+                <p>For a better experience,</p>
+                <p>please use desktop version </p>
+            </div>
+
+            <div className="background">
+                
+            </div>
+        </div>
+    )
+}
+
+export default NoDesktop
