2fed76cc2dce9a1df764723e363ecddd47512652
vahidrza <vrzayev79@gmail.com>
2023-09-01 19:59:16 +0400

    for Ali remove NoDesktop

--- src/Components/Container/Container.jsx
+++ src/Components/Container/Container.jsx
@@ -2,34 +2,34 @@ import React, { useEffect } from "react";
 import "./container.css";
 import Nav from "./Nav/Nav";
 import Main from "./Main/Main";
-import NoDesktop from "./NoDesktop/NoDesktop";
+// import NoDesktop from "./NoDesktop/NoDesktop";
 
 function Container() {
 
-  const mediaQuery = window.matchMedia('(min-width:1200px)');
-  function applyCustomStyles(mql) {
-    if (mql.matches) {
+  // const mediaQuery = window.matchMedia('(min-width:1200px)');
+  // function applyCustomStyles(mql) {
+  //   if (mql.matches) {
       return <>
         <Nav />
         <Main />
       </>
-    } else {
-      return <NoDesktop/>
-    }
-  }
+  //   } else {
+  //     return <NoDesktop/>
+  //   }
+  // }
 
-  useEffect(() => {
-    applyCustomStyles(mediaQuery);
-      mediaQuery.addListener(applyCustomStyles);
-  });
+  // useEffect(() => {
+  //   applyCustomStyles(mediaQuery);
+  //     mediaQuery.addListener(applyCustomStyles);
+  // });
   
-  return (
-    <div className="container">
-      {/* <Nav />
-      <Main /> */}
-      {applyCustomStyles(mediaQuery)}
-    </div>
-  );
+  // return (
+  //   <div className="container">
+  //     {/* <Nav />
+  //     <Main /> */}
+  //     {applyCustomStyles(mediaQuery)}
+  //   </div>
+  // );
 }
 
 export default Container;
