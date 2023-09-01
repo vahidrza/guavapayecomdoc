e225119b9cb7850dc580d17c5060d2d46839f1ec
vahidrza <vrzayev79@gmail.com>
2023-09-01 20:08:56 +0400

    zz

--- src/Components/Container/Container.jsx
+++ src/Components/Container/Container.jsx
@@ -1,4 +1,5 @@
-import React, { useEffect } from "react";
+import React from "react";
+// import React, { useEffect } from "react";
 import "./container.css";
 import Nav from "./Nav/Nav";
 import Main from "./Main/Main";
